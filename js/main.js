a3faa83ca4985ca8e2b12a4f1e8774df9cab82ff
John Coote <john_coote@rocketmail.com>
2018-01-07 22:13:02 +1100

    changed append8

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ console.log("connected");
 
 let APIKEY = "TXJrSO0rz7ST0LSBMfpANGP5EzR2"
 
-$.get(`https://cricapi.com/api/matches?apikey=${APIKEY}`, function(matchdata) {
+$.get(`http://cricapi.com/api/matches?apikey=${APIKEY}`, function(matchdata) {
   const matches = matchdata.matches;
   const intMatches = matches.filter(m => ((m.type === "Test") || (m.type === "ODI")) && (m.matchStarted === true))
 
   $.each(intMatches, function (index, value) {
     matchHeadline = `<div class="matchInfo ${value['type']}" id="${value['unique_id']}">${value['team-1']} vs ${value['team-2']}</div><div class="scores"></div>`
     $('#matchList').append(matchHeadline)
+
+    $.get(`https://cricapi.com/api/cricketScore?apikey=${APIKEY}&unique_id=${matchID}`, function(matchdata) {
+      // console.log(matchdata.description);
+      // console.log($(`#${matchID}`).html());
+      $(`#${matchID}`).next().html(`<div>${matchdata.description}</div>`)
+      // $('h4').html(`${matchdata.description}`)
+
+    });
 
   })
 });
@@ -29,7 +37,7 @@ $(document).ready( function(){
   $('#matchList').on('click touch', function (e) {
     let matchID = e.target.id
     // $('h4').html(matchID)
-    $.get(`https://cricapi.com/api/cricketScore?apikey=${APIKEY}&unique_id=${matchID}`, function(matchdata) {
+    $.get(`http://cricapi.com/api/cricketScore?apikey=${APIKEY}&unique_id=${matchID}`, function(matchdata) {
       // console.log(matchdata.description);
       // console.log($(`#${matchID}`).html());
       // $(`#${matchID}`).next().html(`<div>${matchdata.description}</div>`)
