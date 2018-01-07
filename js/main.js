4a7917749b80f7f678b2844c8559818618681035
John Coote <john_coote@rocketmail.com>
2018-01-07 20:19:27 +1100

    changed append3

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(document).ready( function(){
     update(matchID)
   });
 
-  $('#matchList').on('click touch', function (e) {
+  $('#matchList').on('click touch touchstart', function (e) {
     let matchID = e.target.id
     $.get(`http://cricapi.com/api/cricketScore?apikey=${APIKEY}&unique_id=${matchID}`, function(matchdata) {
       // console.log(matchdata.description);
