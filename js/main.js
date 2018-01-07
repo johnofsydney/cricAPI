48cb305999702095f3b53666f27566861fc265fb
John Coote <john_coote@rocketmail.com>
2018-01-07 18:01:58 +1100

    done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 console.log("connected");
 
+let APIKEY = "TXJrSO0rz7ST0LSBMfpANGP5EzR2"
 
-$.get("https://cricapi.com/api/matches?apikey=TXJrSO0rz7ST0LSBMfpANGP5EzR2", function(matchdata) {
+$.get(`https://cricapi.com/api/matches?apikey=${APIKEY}`, function(matchdata) {
   const matches = matchdata.matches;
   const intMatches = matches.filter(m => ((m.type === "Test") || (m.type === "ODI")) && (m.matchStarted === true))
 
-
   $.each(intMatches, function (index, value) {
-    // console.log( index, value);
-    // console.log(value['team-1']);
-    // console.log(value['team-2']);
-    // console.log(value['unique_id']);
-
-    matchInfo = `<div class="${value['type']}" id="${value['unique_id']}"<h2>${value['team-1']} vs ${value['team-2']}</h2></div>`
-
-    $('#matchList').append(matchInfo)
+    matchHeadline = `<div class="matchInfo ${value['type']}" id="${value['unique_id']}">${value['team-1']} vs ${value['team-2']}</div>`
+    $('#matchList').append(matchHeadline)
   })
 });
+
+
+
+$(document).ready( function(){
+  console.log("ready");
+
+  $('#matchList').on('click', function (e) {
+    // console.log(e.target);
+    // console.log(e.target.tagName);
+    // console.log(e.target.id);
+    let matchID = e.target.id
+    // $(`#${matchID}`).append('<div>isiascij</div>')
+
+    $.get(`http://cricapi.com/api/cricketScore?apikey=${APIKEY}&unique_id=${matchID}`, function(matchdata) {
+      console.log(matchdata.description);
+      $(`#${matchID}`).append(`<div>${matchdata.description}</div>`)
+
+    });
+
+
+
+
+
+
+  })
+})
